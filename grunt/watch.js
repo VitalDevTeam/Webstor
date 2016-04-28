590736d72f4e331ab9b40da09fb283bf16c42a71
Matthew Chase <matt@vtldesign.com>
2016-04-28 12:50:03 -0400

    Added a bit of demo content

--- grunt/watch.js
+++ grunt/watch.js
@@ -8,6 +8,10 @@ module.exports = {
 		files: ['src/sass/**/*.scss'],
 		tasks: ['css']
 	},
+	images: {
+		files: ['src/images/**/*.{gif,jpg,jpeg,png,svg}'],
+		tasks: ['image']
+	},
 	content: {
 		files: ['src/content/**/*'],
 		tasks: ['copy']
